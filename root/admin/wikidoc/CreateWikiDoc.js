bf7c7911729b5e94f57bea4ef687214a9f3c4053
yvanzo <yvanzo@metabrainz.org>
2020-04-23 09:35:27 +0200

    Eat all of reo's virtual chocolate

--- root/admin/wikidoc/CreateWikiDoc.js
+++ root/admin/wikidoc/CreateWikiDoc.js
@@ -1,8 +1,7 @@
 /*
  * @flow
  * Copyright (C) 2020 MetaBrainz Foundation
- *      \  á))  s  .'
- *       '-.((*_.-'
+ *
  * This file is part of MusicBrainz, the open internet music database,
  * and is licensed under the GPL version 2, or (at your option) any
  * later version: http://www.gnu.org/licenses/gpl-2.0.txt
--- root/admin/wikidoc/types.js
+++ root/admin/wikidoc/types.js
@@ -1,8 +1,7 @@
 /*
  * @flow strict
  * Copyright (C) 2020 MetaBrainz Foundation
- *          ___
- *       .-*)) `*-.
+ *
  * This file is part of MusicBrainz, the open internet music database,
  * and is licensed under the GPL version 2, or (at your option) any
  * later version: http://www.gnu.org/licenses/gpl-2.0.txt
